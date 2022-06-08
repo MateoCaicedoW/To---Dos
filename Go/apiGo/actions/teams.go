99049ab61f11c623b6f9c8caae300e624b2c29f1
MateoCaicedoW <mcaicedo@wawand.co>
2022-06-08 16:28:26 -0500

    movements and test

--- Go/apiGo/actions/teams.go
+++ Go/apiGo/actions/teams.go
@@ -163,7 +163,7 @@ func findTeam(handler handler, ID string, w http.ResponseWriter, response models
 
 	if result := handler.db.First(&team, &ID); result.Error != nil {
 		w.WriteHeader(http.StatusNotFound)
-		response.Message = team.Name + " not found"
+		response.Message = "Team not found"
 		response.Status = http.StatusNotFound
 		json.NewEncoder(w).Encode(response)
 		err = errors.New("team not found")
